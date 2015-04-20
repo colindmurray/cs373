28db52f7175b95627f3f65b5623ddf9e465e52f9
Glenn Downing <downing@cs.utexas.edu>
2015-04-19 20:13:40 -0500

    another commit

---
+++ java/SingletonPattern.java
@@ -0,0 +1,41 @@
+// ---------------------
+// SingletonPattern.java
+// ---------------------
+
+class Eager {
+    private static final Eager _only = new Eager();
+
+    private Eager ()
+        {}
+
+    public static Eager only () {
+        return _only;}
+
+    public String f () {
+        return "Eager.f()";}}
+
+class Lazy {
+    private static Lazy _only;
+
+    private Lazy ()
+        {}
+
+    public static Lazy only () {
+        if (Lazy._only == null)
+            Lazy._only = new Lazy();
+        return Lazy._only;}
+
+    public String f () {
+        return "Lazy.f()";}}
+
+final class SingletonPattern {
+    public static void main (String[] args) {
+        System.out.println("SingletonPattern.java");
+
+        assert Eager.only()     == Eager.only();
+        assert Eager.only().f() == "Eager.f()";
+
+        assert Lazy.only()     == Lazy.only();
+        assert Lazy.only().f() == "Lazy.f()";
+
+        System.out.println("Done.");}}
